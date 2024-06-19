ecf0bcd84d7236a7b4f2d3117c4e0b88ddef64ac
Darwinsalcie <149649911+Darwinsalcie@users.noreply.github.com>
2024-06-19 18:16:03 -0400

    75%

--- Shop.Web/Program.cs
+++ Shop.Web/Program.cs
@@ -1,6 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using ShopApp.DAL.Context;
+using ShopApp.DAL.Daos;
+using ShopApp.DAL.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//registrar el contexto//
+
+builder.Services.AddDbContext<ShopAppContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("ShopAppContext")));
+
+builder.Services.AddScoped<ICategoriesDb, CategoryDao>();
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
